07edd4c6fa44c5177fe0acf12c6dc77d5eee9afd
Claire Buck <ecbuck@me.com>
2019-12-05 18:31:20 -0700

    Updated for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const DAL = require("./dataAccessLayer");
 require("dotenv").config();
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 
